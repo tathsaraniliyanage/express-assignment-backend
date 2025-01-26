dfd53590b64bf43a38606e6702727b20e4172514
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2025-01-26 16:30:06 +0530

    Customer section completed

---
+++ database/local-data-store.ts
@@ -0,0 +1,25 @@
+import Customer from "../model/Customer";
+
+let customers : Customer[] = [];
+
+export function addCustomer(customer: Customer) {
+    customers.push(customer);
+}
+
+export function getCustomers() {
+    return customers;
+}
+
+export function deleteCustomer(id: number) {
+    customers = customers.filter(customer => customer.id !== id);
+}
+
+export function updateCustomer(id: number, c: Customer) {
+    customers.map((customer: Customer) => {
+        if(customer.id === id) {
+            customer.name = c.name;
+            customer.nic = c.nic;
+            customer.email = c.email;
+        }
+    })
+}
